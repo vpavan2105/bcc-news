06ce4ce622d699beeb124fd842d3bd2f01e6af36
PoornimaMohabe <147687634+PoornimaMohabe@users.noreply.github.com>
2024-02-21 23:55:53 +0530

    first

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { Box, SimpleGrid } from '@chakra-ui/react'
 import './App.css'
 import CardComponent from './components/CardComponent'
+import Login from './components/Login'
 
 function App() {
 
   return (
     <>
     <SimpleGrid columns={[2, null, 4]} spacing='40px' px={"150px"}>
+      {/* <CardComponent/>
       <CardComponent/>
       <CardComponent/>
-      <CardComponent/>
-      <CardComponent/>
+      <CardComponent/> */}
     </SimpleGrid>
+    <Login/>
     </>
   )
 }
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div style={{border:"2px solid red"}}>
+            {/* FLEX 1 */}
+            <div>
+                <img src="" alt="" />
+                <h2>Sign in with your email or username</h2>
+                <form action="">
+                    <input type="text" placeholder='Email or username' />
+                    <button>Next</button>
+
+                </form>
+                <a href="">Need help signing in?</a>
+
+                <hr />
+                <h4>Don't have a BBC account?</h4>
+                <a href="">Register now</a>
+
+            </div>
+
+            {/*  FLEX 2 */}
+            <div>
+<img src="https://t4.ftcdn.net/jpg/03/33/68/01/240_F_333680128_5XjB38xRZLApJiVNFRozfJ5peKlzOmiJ.jpg" alt="" />
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXd4IdSPcW9PhJ4J01qYo-0j2Ep874uzPzkVZHSxoNYCw9hBq2yoQ7xavyfh3qQnpPLH0&usqp=CAU" alt="" />
+<img src="https://images.pexels.com/photos/433452/pexels-photo-433452.jpeg?cs=srgb&dl=pexels-pixabay-433452.jpg&fm=jpg" alt="" />
+<img src="https://www.shutterstock.com/image-photo/bird-photography-picture-most-beautiful-260nw-2401171763.jpg" alt="" />
+
+            </div>
+        </div>
+    )
+}
+
+export default Login
