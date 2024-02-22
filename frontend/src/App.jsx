2190ae26abd7a066e0a13a4a92df7ff9abe830a7
sachinkumanache <kumanache@gmail.com>
2024-02-22 16:20:04 +0530

    navbar imported

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
-import './App.css'
-import CardComponent from './components/CardComponent'
-import Login from './components/Login'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './Pages/HomePage'
+import "./App.css";
+import CardComponent from "./components/CardComponent";
+import Login from "./components/Login";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import { SimpleGrid } from "@chakra-ui/react";
 
 function App() {
-
   return (
     <>
-    <SimpleGrid columns={[2, null, 4]} spacing='40px' px={"150px"}>
-      <CardComponent/>
-      <CardComponent/>
-      <CardComponent/>
-      <CardComponent/>
-    </SimpleGrid>
-    <Login/>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      {/* <Route path='/login' element={<Login/>}/>
+      <SimpleGrid columns={[2, null, 4]} spacing="40px" px={"150px"}>
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+      </SimpleGrid>
+      <Login />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path='/login' element={<Login/>}/>
       <Route path='/general' element={<General/>}/>
       <Route path='/sports' element={<Sports/>}/>
       <Route path='/business' element={<Business/>}/>
@@ -26,9 +26,9 @@ function App() {
       <Route path='/science' element={<Science/>}/>
       <Route path='/politics' element={<Politics/>}/>
       <Route path='/*' element={<SingleNewsPage/>}/> */}
-    </Routes>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Politics.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+function Politics() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default Politics;
