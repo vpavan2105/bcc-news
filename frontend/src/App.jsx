0863b500dbfc464c3c12f50f1fa73cd24e12c267
sachinkumanache <kumanache@gmail.com>
2024-02-22 21:36:46 +0530

    politics page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 
 import "./App.css";
 import CardComponent from "./components/CardComponent";
-import Login from "./components/Login";
+import Login from "./components1/Login";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import { SimpleGrid } from "@chakra-ui/react";
+import Politics from "./Pages/Politics";
 
 
 function App() {
@@ -16,30 +17,19 @@ function App() {
       <CardComponent/>
       <CardComponent/> */}
     </SimpleGrid>
-    <Login/>
-    
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      {/* <Route path='/login' element={<Login/>}/>
-    <> 
-      <SimpleGrid columns={[2, null, 4]} spacing="40px" px={"150px"}>
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-        <CardComponent />
-      </SimpleGrid>
-      <Login />
+    {/* <Login/> */}
+
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path='/login' element={<Login/>}/>
-      <Route path='/general' element={<General/>}/>
-      <Route path='/sports' element={<Sports/>}/>
-      <Route path='/business' element={<Business/>}/>
-      <Route path='/health' element={<Health/>}/>
-      <Route path='/entertainment' element={<Entertainment/>}/>
-      <Route path='/science' element={<Science/>}/>
-      <Route path='/politics' element={<Politics/>}/>
-      <Route path='/*' element={<SingleNewsPage/>}/> */}
+        <Route path='/login' element={<Login/>}/>
+        {/* <Route path='/general' element={<General/>}/>
+        <Route path='/sports' element={<Sports/>}/>
+        <Route path='/business' element={<Business/>}/>
+        <Route path='/health' element={<Health/>}/>
+        <Route path='/entertainment' element={<Entertainment/>}/>
+        <Route path='/science' element={<Science/>}/> */}
+        <Route path='/politics' element={<Politics/>}/>
+        {/* <Route path='/*' element={<SingleNewsPage/>}/> */}
       </Routes>
     </>
   );
