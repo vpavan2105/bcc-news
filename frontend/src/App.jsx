1f26f733d83a79a52a129e7fba2ed0833d2a08ce
sachinmohite <sachinmohite708@gmail.com>
2024-02-23 19:28:55 +0530

    emptyfile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import { Box, SimpleGrid } from '@chakra-ui/react'
-import './App.css'
-import CardComponent from './components/CardComponent'
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import "./App.css";
+import CardComponent from "./components/CardComponent";
+import Sport from "./components/Sport";
 
 function App() {
-
   return (
     <>
-    <SimpleGrid columns={[2, null, 4]} spacing='40px' px={"150px"}>
-      <CardComponent/>
-      <CardComponent/>
-      <CardComponent/>
-      <CardComponent/>
-    </SimpleGrid>
+      <SimpleGrid columns={[2, null, 4]} spacing="40px" px={"150px"}>
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+        <CardComponent />
+      </SimpleGrid>
+      <Sport />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
