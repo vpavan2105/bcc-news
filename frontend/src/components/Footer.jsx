4ae98bc7a6ede38c1715667a35ce2a48001d24a6
sachinmohite <sachinmohite708@gmail.com>
2024-02-25 20:18:19 +0530

    addingpop

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Box, Center, Divider, Flex, Square, Text } from "@chakra-ui/react";
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
