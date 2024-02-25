a898d86baf5026cfe39f7d494bf5ee4ee31a40c9
Saurabh Ganguly <saurabhganguly38@gmail.com>
2024-02-26 01:19:18 +0530

    login

--- frontend/src/Routes/PrivateRoute.jsx
+++ frontend/src/Routes/PrivateRoute.jsx
@@ -2,9 +2,9 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default  function PrivateRoute({children}) {
-    let { login } = useSelector((state) => state.login)
+    let isAuth = useSelector((state) => state.login)
 
-    return login.isAuth ?  children : <Navigate to={"/login"}/>
+    return isAuth ?  children : <Navigate to={"/login"}/>
 	
 }
 
